4a6b72cb6fc2f5b8ce6ec07c4015caa87f69cfd0
dianecasanova <festusnova1989@gmail.com>
2021-01-14 19:29:40 +0100

    Update Db

--- prisma/migrations/20210114182820_/migration.sql
+++ prisma/migrations/20210114182820_/migration.sql
@@ -1,22 +1,11 @@
 -- CreateTable
-CREATE TABLE `step` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `city` VARCHAR(191) NOT NULL,
-    `duration` INT,
-    `description` VARCHAR(191),
-    `media_path` VARCHAR(191),
-    `id_trip` INT NOT NULL,
-INDEX `step_user_FK`(`id_trip`),
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `trip` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `title` VARCHAR(191) NOT NULL,
-    `duration` INT,
+    `Date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `description` VARCHAR(191),
+    `gps` INT NOT NULL,
+    `media_path` VARCHAR(191),
     `id_user` INT NOT NULL,
 INDEX `trip_user_FK`(`id_user`),
 
@@ -30,12 +19,10 @@ CREATE TABLE `user` (
     `lastname` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
+UNIQUE INDEX `user.email_unique`(`email`),
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `step` ADD FOREIGN KEY (`id_trip`) REFERENCES `trip`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `trip` ADD FOREIGN KEY (`id_user`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
